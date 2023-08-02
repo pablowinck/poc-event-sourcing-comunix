acb036a0e2ba369a2410ccdc4f64ae5a0851d5b1
pablowinck <pablowinck123@gmail.com>
2023-08-02 16:39:05 -0300

    feat: test config

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { InboundConfiguration } from './inbound/config/InboundConfiguration.js'
 const { server, eventManager } = new CoreConfiguration()
 new InboundConfiguration(eventManager);
 
-
 server.listen(3000, () => {
     console.log('Server started')
 })
---
+++ test/core/entity/EventManager.test.js
@@ -0,0 +1,13 @@
+import { describe, it, mock } from 'node:test'
+import assert from 'node:assert'
+import { EventManager } from '../../../src/core/entity/EventManager.js';
+
+describe('EventManager', () => {
+    it('quando chamo o método emit, ele deve emitir o evento "event-proxy"', () => {
+        const mockfn = mock.fn()
+        const eventManager = new EventManager()
+        eventManager.on('event-proxy', mockfn)
+        eventManager.emit('user-created', { name: 'John Doe' })
+        assert.strictEqual(mockfn.mock.calls.length, 1)
+    });
+})
